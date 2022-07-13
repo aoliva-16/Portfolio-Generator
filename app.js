c4c167c9efd71ce4eeb1f6ae11288a55734bf459
Angelica Olivares <olivares_199@outlook.com>
2022-07-12 18:08:04 -0700

    Validations added

--- app.js
+++ app.js
@@ -7,12 +7,28 @@ const promptUser = () => {
         {
             type: 'input', 
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input', 
             name: 'github', 
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username (Required)',
+            validate: githubUsername => {
+                if (githubUsername) {
+                    return true;
+                } else {
+                    console.log('Please enter your Github username!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input', 
@@ -41,12 +57,28 @@ const promptProject = portfolioData => {
         {
             type: 'input', 
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required)', 
+            validate: projectName => {
+                if (projectName) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description', 
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if (projectDescription) {
+                    return true;
+                } else {
+                    console.log('Please enter a project description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -57,7 +89,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your projecy. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            validate: githubLink => {
+                if (githubLink) {
+                    return true;
+                } else {
+                    console.log('Please enter a Github link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm', 
